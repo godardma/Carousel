d66ee7d20455ade3c3e411e7d2ada7d472a6d482
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-06-29 15:44:46 +0200

    feature: add simple_simulator_with_neural_net.py

--- main_train_network.py
+++ main_train_network.py
@@ -1,9 +1,8 @@
+import PyQt6
 from torch import from_numpy, Tensor
 
 from datasets.get_dataset_v1 import get_dataset_v1
 from neural_net_pytorch_ import NeuralNetwork
-
-
 def cost_fn(prediction, true):
     return ((prediction - true) ** 2).sum()
 
@@ -15,7 +14,7 @@ def train_network(inputs, outputs, dataset_name: str, epochs: int):
     model.train_model(X, y, cost_fn=cost_fn, epochs=epochs)
     model.register_to_csv(f"models/network_{dataset_name}_{epochs}.csv")
 
-def main_train_network(version: int = 1, N: int = 100, epochs: int = 100):
+def main_train_network(version: int = 1, N: int = 1000, epochs: int = 1000):
     inputs, outputs = get_dataset_v1(N)
     dataset_name = f'v{version}_{N}'
     train_network(inputs, outputs, dataset_name, epochs)
